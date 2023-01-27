f5034824c7f11e86d428aaf59723215f65da284e
Sakib Rahman <85602964+thesakibrahman@users.noreply.github.com>
2023-01-27 15:45:29 +0530

    Update Hello.c

--- C programing/Hello.c
+++ C programing/Hello.c
@@ -47,5 +47,8 @@ int main()
     scanf("%[^\n]%*c", &s);
   	
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+	printf("Hello, World!\n");
+    printf("%s",s);
+	
     return 0;
 }
